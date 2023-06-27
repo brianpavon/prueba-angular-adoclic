3cc78f43a14e1407e8e1994a8abdbe44b9b1ab6b
53911453+brianpavon@users.noreply.github.com <53911453+brianpavon@users.noreply.github.com>
2023-06-27 12:32:31 -0300

    ok jokes

--- src/app/pages/dog-joke/dog-joke.component.ts
+++ src/app/pages/dog-joke/dog-joke.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { JokesService } from 'src/app/services/jokes.service';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-dog-joke',
   templateUrl: './dog-joke.component.html',
   styleUrls: ['./dog-joke.component.css']
 })
-export class DogJokeComponent implements OnInit {
+export class DogJokeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  joke : string = '';
+  subscription !: Subscription;
+
+  constructor(private jokeServ : JokesService) { }
 
   ngOnInit(): void {
+    this.getJokes();
+    this.subscription = interval(5000).subscribe(() => {
+      this.getJokes();
+    });
+  }
+  
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  getJokes(){
+    this.jokeServ.getJoke().subscribe(data=>{
+      this.joke = `${data.setup} ${data.punchline}`
+    })
+  }
+  
+  refreshJokes() {
+    this.subscription.unsubscribe();
+    this.getJokes();
+    this.subscription = interval(5000).subscribe(() => {
+      this.getJokes();
+    });
   }
 
 }
--- src/app/services/jokes.service.ts
+++ src/app/services/jokes.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, interval } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JokesService {
+  url : string = 'https://official-joke-api.appspot.com/random_joke';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  
+  // getJoke(): Observable<any> {
+  //   return interval(20000).pipe(
+  //     switchMap(() => 
+  //       this.http.get(this.url)
+  //     )
+  //   );
+  // }
+  getJoke():Observable<any>{
+    return this.http.get(this.url);
+  }
 }
